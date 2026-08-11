349916c56b498e55e806d5147674279dd4a8c0ac
93Samara <93samara.santos@gmail.com>
2026-08-11 17:29:08 -0300

    Adiciona sistema de livraria

---
+++ livraria.py
@@ -0,0 +1,77 @@
+def addLivro(dicLivros):
+        print("=== Adicionar livro ===")
+ 
+        titulo = input("\n Título: ").lower().strip()
+        quantidade = int(input("\n Quantidade: "))
+ 
+        if titulo in dicLivros:
+            dicLivros[titulo] += quantidade
+        else:
+            dicLivros[titulo] = quantidade
+ 
+        print("Livro adicionado!")
+ 
+def removeLivro(dicLivros):
+        print("=== Remover Livro! ===")
+        titulo = input("\n Título: ").lower().strip()
+ 
+        if titulo not in dicLivros:
+                print("\n Titulo não encontrado")
+        else:
+            quantidade = int(input("\nQuantidade"))
+            if quantidade > dicLivros[titulo]:
+                print("Quantidade insuficiente ") 
+                print (f"Quantidade disponível {dicLivros[titulo]}")
+            else:
+                dicLivros[titulo] -= quantidade
+                print (f"Quantidade atualizada: {dicLivros[titulo]}")  
+ 
+def consulta(dicLivros):
+        titulo = input("\n Digite o título do livro: ").lower().strip()
+        if titulo in dicLivros:
+            print (f"\n Livro: {titulo}")
+            print (f"Quantidade disponível {dicLivros[titulo]}")
+        else:
+            print("Livro não encontrado!")
+ 
+def listar(dicLivros):
+        print("\n === Livros disponíveis ===")
+ 
+        for titulo, quantidade in dicLivros.items():
+            print(f"{titulo} - {quantidade}")
+ 
+def main():
+ 
+    dicLivros = {
+            "querido john": 12,
+            "a ultima musica": 6
+            }
+ 
+    while True:
+ 
+        print(" \n=== SISTEMA DE GESTÃO DE LIVRARIA ===")
+        print(" \n Escolha uma opção" )
+        print(" 1 - Adicionar Livro")
+        print(" 2 - Remover Livro")
+        print(" 3 - Consultar Livro")
+        print(" 4 - Listar Livros")
+        print(" 5 - Sair")
+ 
+        op = input("\n Digite a opção escolhida: ")
+ 
+        match op:
+             case "1":
+                  addLivro(dicLivros)
+             case "2":
+                  removeLivro(dicLivros)
+             case "3":
+                  consulta(dicLivros)
+             case "4":
+                  listar(dicLivros)
+             case "5":
+                  print("Saindo...")
+                  break
+             case _:
+                  print("\n Opção inválida")
+ 
+main ()
